e037d394cec6739432ffcf30b27c88e2a8fff0d8
Sirishabonam <89244810+Sirishabonam@users.noreply.github.com>
2021-08-30 15:55:53 +0530

    Add files via upload

--- AreaOfaTriangle.java
+++ AreaOfaTriangle.java
@@ -1,26 +1,35 @@
-package Programs;
-
-import java.util.Scanner;
-
-public class AreaOfaTriangle {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-
-	      System.out.println("Enter the width of the Triangle:");
-	      double base = scanner.nextDouble();
-
-	      System.out.println("Enter the height of the Triangle:");
-	      double height = scanner.nextDouble();
-
-	      //Area = (width*height)/2
-	      double area = (base* height)/2;
-	      System.out.println("Area of Triangle is: " + area);      
-
-
-	}
-
-
-}
+package assignments_SDET; 
+2 
+ 
+3 import java.util.Scanner; 
+4 
+ 
+5 public class AreaOfaTriangle { 
+6 	public static void main(String[] args) { 
+7 		// TODO Auto-generated method stub 
+8 		 
+9 		@SuppressWarnings("resource") 
+10 		Scanner scanner = new Scanner(System.in); 
+11 
+ 
+12 	      System.out.println("Enter the width of the Triangle:"); 
+13 	      double base = scanner.nextDouble(); 
+14 
+ 
+15 	      System.out.println("Enter the height of the Triangle:"); 
+16 	      double height = scanner.nextDouble(); 
+17 
+ 
+18 	      //Area = (width*height)/2 
+19 	      double area = (base* height)/2; 
+20 	      System.out.println("Area of Triangle is: " + area);       
+21 
+ 
+22 
+ 
+23 	} 
+24 
+ 
+25 
+ 
+26 } 
